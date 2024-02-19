b085a40ddc19ccea2b510536a53782793a09cf4a
Yanjun Liu <yanjunlbcit@gmail.com>
2024-02-18 21:00:10 -0800

    adding onboarding step3 and step4

--- src/pages/onboarding.tsx
+++ src/pages/onboarding.tsx
@@ -1,5 +1,6 @@
 import Image  from 'next/image'
 import styles from '@/styles/onboarding.module.css'
+import Link from 'next/link';
 import { useState } from 'react';
 
 
@@ -9,7 +10,7 @@ export default function Onboarding() {
 
   return (
     <main className={styles.main}>
-      <div className={styles.mainContainer}>
+      <div className={`${styles.mainContainer} ${styles.background}`}>
         <div className={styles.heading}>
             <p>Orbit.</p>
         </div>
@@ -74,7 +75,9 @@ export default function Onboarding() {
                     width={642}
                     alt="location graphic"
                 />
-                <button className={styles.button}>Submit</button>
+                <Link href="/onboarding_3">
+                    <button className={styles.button}>Submit</button>
+                </Link>
             </div>
         </div>
       </div>
