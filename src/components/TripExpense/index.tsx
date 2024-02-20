36f176ea5349fb21b91a2b299d8c5fa7bccb91a2
Juliet Kim <julietkim@Juliets-Laptop.local>
2024-02-19 20:30:08 -0800

    results page made with Itinerary component and Tripexpense component

---
+++ src/components/TripExpense/index.tsx
@@ -0,0 +1,10 @@
+import styles from "@/components/TripExpense/TripExpense.module.css";
+import Image from "next/image";
+
+export default function TripExpense() {
+  return (
+    <main className={`${styles.mainContainer} ${styles.background}`}>
+     
+    </main>
+  );
+}
