0950eef526990b91f9ba179506f2d3d53acd29fd
Jimmy Lam <jimmylam@Jimmys-MacBook-Pro.local>
2024-02-20 10:35:25 -0800

    api edits

---
+++ src/components/weather.ts
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import { WeatherStats } from '../typings.d' ;
+
+export async function fetchWeather(city: string): Promise<WeatherStats | null> {
+  const apiKey = process.env.NEXT_WEATHER_API;
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  try {
+    const response = await axios.get(apiUrl);
+    return response.data as WeatherStats;
+  } catch (error) {
+    console.error('Error fetching weather:', error);
+    return null;
+  }
+}
